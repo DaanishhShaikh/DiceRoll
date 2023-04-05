bd99566bfe6195e02a7a67cf23b3a094cf56c8eb
Danish Shaikh <90503323+DaanishhShaikh@users.noreply.github.com>
2023-04-05 18:21:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+window.onload = function() {
+  var message = "Welcome Please Refresh the page and let the site roll the dice! Trust me it's random(wink wink!)";
+  alert(message);
+};
+
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
